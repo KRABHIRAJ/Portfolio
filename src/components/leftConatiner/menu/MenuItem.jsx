32da6b0da93703d1a1135b3d12ad26c0ed100566
Abhiraj Kumar <kumarshishu99@gmail.com>
2024-08-28 07:39:24 +0530

    width issue

--- src/components/leftConatiner/menu/MenuItem.jsx
+++ src/components/leftConatiner/menu/MenuItem.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 const MenuItem = ({title}) => {
   return (
-    <div className="group cursor-pointer flex items-center gap-x-2">
+    <div className="group cursor-pointer flex items-center gap-x-2 px-2 w-fit">
         <div className="h-[2px] w-8 group-hover:w-16 bg-slate-600 group-hover:bg-slate-200 transition-all" />
         <p className="group-hover:text-slate-200 text-slate-500 text-xs uppercase font-bold tracking-widest transition-all">{title}</p>
     </div>
